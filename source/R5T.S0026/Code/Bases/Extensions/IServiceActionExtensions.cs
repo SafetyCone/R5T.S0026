3fca46664e31b498df42b773d00f19bd8c97bdf6
David Coats <david@getsafetycone.com>
2021-12-15 12:39:59 -0800

    Initial commit of program-as-a-service using R5T.A0003 services platform.

---
+++ source/R5T.S0026/Code/Bases/Extensions/IServiceActionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using R5T.T0062;
+using R5T.T0063;
+
+
+namespace R5T.S0026
+{
+    public static class IServiceActionExtensions
+    {
+        public static IServiceAction<HostStartup> AddStartupAction(this IServiceAction _)
+        {
+            var output = _.New<HostStartup>(services => services.AddHostStartup());
+
+            return output;
+        }
+    }
+}
---
+++ source/R5T.S0026/Code/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+
+using R5T.D0088;
+using R5T.D0090;
+
+
+namespace R5T.S0026
+{
+    public class Program : ProgramAsAServiceBase
+    {
+        #region Static
+
+        static async Task Main()
+        {
+            //OverridableProcessStartTimeProvider.Override("20211214-163052");
+            //OverridableProcessStartTimeProvider.DoNotOverride();
+
+            await Instances.Host.NewBuilder()
+                .UseProgramAsAService<Program, T0075.IHostBuilder>()
+                .UseHostStartup<HostStartup, T0075.IHostBuilder>(Instances.ServiceAction.AddStartupAction())
+                .Build()
+                .SerializeConfigurationAudit()
+                .SerializeServiceCollectionAudit()
+                .RunAsync();
+        }
+
+        #endregion
+
+
+        public Program(IServiceProvider serviceProvider)
+            : base(serviceProvider)
+        {
+        }
+
+        protected override Task ServiceMain(CancellationToken stoppingToken)
+        {
+            return this.RunOperation();
+        }
+
+        private async Task RunOperation()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
